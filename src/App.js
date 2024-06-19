69c7e564e65249ef82cadfc2d11bd44c7d9091cd
upasana0511 <bhattupasana1998@gmail.com>
2024-06-19 18:18:29 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react"
+import Cell from "./components/Cell"
+const App = () => {
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""])
+  const [go, setGo] = useState("circle")
+  const [winningMessage, setWinningMessage] = useState(null)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const message = "It is now " + go + "'s go."
+
+    const checkScore = () => {
+      const winningCombos = [
+          [0, 1, 2], [3, 4, 5],
+          [6, 7, 8], [0, 3, 6],
+          [1 ,4, 7], [2, 5, 8],
+          [0, 4, 8], [2, 4, 6]
+      ]
+        winningCombos.forEach(array =>{
+            let circleWins = array.every(cell => cells[cell] === "circle")
+            if(circleWins) {
+                setWinningMessage("Circle Wins!")
+                return
+            }
+        })
+        winningCombos.forEach(array =>{
+            let crossWins = array.every(cell => cells[cell] === "cross")
+            if(crossWins) {
+                setWinningMessage("cross Wins!")
+                return
+            }
+        })
+    }
+
+    useEffect(() => {
+            checkScore()
+    }, [cells]);
+
+    const resetGame = () => {
+        setCells(["", "", "", "", "", "", "", "", ""]);
+        setGo("circle");
+        setWinningMessage(null);
+    };
+
+    return (
+        <div className="app">
+            <h1 className="heading">TIC-TAC-TOE</h1>
+            <div className="gameboard">
+                {cells.map((cell, index) => (
+                    <Cell
+                        key={index}
+                        id={index}
+                        cell={cell}
+                        go={go}
+                        setGo={setGo}
+                        cells={cells}
+                        setCells={setCells}
+                        winningMessage={winningMessage}
+                    />
+                ))}
+            </div>
+            <p>{winningMessage || message}</p>
+            {winningMessage && <button className="play-again-button" onClick={resetGame}>Play Again</button>}
+            <div className="footer">
+                Created by Upasana Bhatt
+            </div>
+        </div>
+    );
+
 }
 
 export default App;
